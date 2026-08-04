291ff23bb297fab78b9d5abfe4174b02bb74e610
larissabubola-lab <larissa.bubola@escola.pr.gov.br>
2026-08-04 19:47:13 -0300

    Site com header simples e cadastro

---
+++ bullying/script.js
@@ -0,0 +1,12 @@
+// const botao_login = document.getElementById("login");
+// const botao_cadastro = document.getElementById("cadastro");
+
+const cadastro_pagina = document.getElementById("pagina_cadastro");
+
+function abrir_cadastro(){
+    cadastro_pagina.style.display = "flex";
+}
+
+function fechar_cadastro(){
+    cadastro_pagina.style.display = "none";
+}
