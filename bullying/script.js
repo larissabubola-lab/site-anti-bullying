3c409cb9c4670826161c0917d62834790015efef
Larissa <larissa.bubola@escola.pr.gov.br>
2026-08-05 10:46:32 -0300

    adicionada as paginas

--- bullying/script.js
+++ bullying/script.js
@@ -20,3 +20,36 @@ function abrir_login(){
 function fechar_login(){
     login_pagina.style.display = "none";
 }
+
+const pagina_inicio = document.getElementById("inicio");
+const pagina_sobre_nos = document.getElementById("sobre_nos");
+const pagina_como_denunciar = document.getElementById("como_denunciar");
+const pagina_contatos = document.getElementById("contatos");
+
+function abrir_inicio(){
+    pagina_inicio.style.display = "flex";
+    pagina_sobre_nos.style.display = "none";
+    pagina_como_denunciar.style.display = "none";
+    pagina_contatos.style.display = "none";
+}
+
+function abrir_sobre_nos(){
+    pagina_inicio.style.display = "none";
+    pagina_sobre_nos.style.display = "flex";
+    pagina_como_denunciar.style.display = "none";
+    pagina_contatos.style.display = "none";
+}
+
+function abrir_como_denunciar(){
+    pagina_inicio.style.display = "none";
+    pagina_sobre_nos.style.display = "none";
+    pagina_como_denunciar.style.display = "flex";
+    pagina_contatos.style.display = "none";
+}
+
+function abrir_contatos(){
+    pagina_inicio.style.display = "none";
+    pagina_sobre_nos.style.display = "none";
+    pagina_como_denunciar.style.display = "none";
+    pagina_contatos.style.display = "flex";
+}
