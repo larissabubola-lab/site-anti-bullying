a22bf864ccaafb000d5744b7619214f3a95a8d7a
larissa <larissa.bubola@escola.pr.gov.br>
2026-08-16 17:22:53 -0300

    arrumado js

--- bullying/script.js
+++ bullying/script.js
@@ -21,7 +21,7 @@ function fechar_login(){
     login_pagina.style.display = "none";
 }
 
-const pagina_inicio = document.getElementById("inicio");
+const pagina_inicio = document.getElementById("main");
 const pagina_sobre_nos = document.getElementById("sobre_nos");
 const pagina_como_denunciar = document.getElementById("como_denunciar");
 const pagina_contatos = document.getElementById("contatos");
@@ -53,3 +53,91 @@ function abrir_contatos(){
     pagina_como_denunciar.style.display = "none";
     pagina_contatos.style.display = "flex";
 }
+
+let slideAtual = 0;
+
+const slides = document.querySelectorAll(".slide");
+
+const indicadores = document.querySelectorAll(".indicador");
+
+let intervaloCarrossel;
+
+function mostrarSlide(numero) {
+
+    slides.forEach(function(slide) {
+
+        slide.classList.remove("ativo");
+
+    });
+
+    indicadores.forEach(function(indicador) {
+
+        indicador.classList.remove("ativo");
+
+    });
+
+    slides[numero].classList.add("ativo");
+
+    indicadores[numero].classList.add("ativo");
+
+    slideAtual = numero;
+
+}
+
+function mudarSlide(direcao) {
+
+    let novoSlide = slideAtual + direcao;
+
+    if (novoSlide >= slides.length) {
+
+        novoSlide = 0;
+
+    }
+
+    if (novoSlide < 0) {
+
+        novoSlide = slides.length - 1;
+
+    }
+
+    mostrarSlide(novoSlide);
+
+    reiniciarCarrossel();
+
+}
+
+function irParaSlide(numero) {
+
+    mostrarSlide(numero);
+
+    reiniciarCarrossel();
+
+}
+
+function iniciarCarrossel() {
+
+    intervaloCarrossel = setInterval(function() {
+
+        let novoSlide = slideAtual + 1;
+
+        if (novoSlide >= slides.length) {
+
+            novoSlide = 0;
+
+        }
+
+        mostrarSlide(novoSlide);
+
+    }, 5500);
+
+}
+
+function reiniciarCarrossel() {
+
+    clearInterval(intervaloCarrossel);
+
+    iniciarCarrossel();
+
+}
+
+iniciarCarrossel();
