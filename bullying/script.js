a2faecaf97cb8d4d931b921b5f5b56909219cb29
larissa <larissa.bubola@escola.pr.gov.br>
2026-08-23 15:11:17 -0300

    melhorado sobre nos e adicionado como denunciar

--- bullying/script.js
+++ bullying/script.js
@@ -1,25 +1,25 @@
 // const botao_login = document.getElementById("login");
 // const botao_cadastro = document.getElementById("cadastro");
 
-const cadastro_pagina = document.getElementById("pagina_cadastro");
+// const cadastro_pagina = document.getElementById("pagina_cadastro");
 
-function abrir_cadastro(){
-    cadastro_pagina.style.display = "flex";
-}
+// function abrir_cadastro(){
+//     cadastro_pagina.style.display = "flex";
+// }
 
-function fechar_cadastro(){
-    cadastro_pagina.style.display = "none";
-}
+// function fechar_cadastro(){
+//     cadastro_pagina.style.display = "none";
+// }
 
-const login_pagina = document.getElementById("pagina_login");
+// const login_pagina = document.getElementById("pagina_login");
 
-function abrir_login(){
-    login_pagina.style.display = "flex";
-}
+// function abrir_login(){
+//     login_pagina.style.display = "flex";
+// }
 
-function fechar_login(){
-    login_pagina.style.display = "none";
-}
+// function fechar_login(){
+//     login_pagina.style.display = "none";
+// }
 
 const pagina_inicio = document.getElementById("main");
 const pagina_sobre_nos = document.getElementById("sobre_nos");
